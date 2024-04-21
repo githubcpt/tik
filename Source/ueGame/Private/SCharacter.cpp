31fe0112265cf5ede97dd4475814a7cefc606258
caopantao <caopantao@partners.poissonsoft.com>
2024-04-21 21:31:15 +0800

    ueGame

---
+++ Source/ueGame/Private/SCharacter.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ASCharacter::ASCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create a spring arm
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	// Attach spring arm to character
+	SpringArmComp->SetupAttachment(RootComponent);
+	
+	// Create a camera 
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	// Attach camera to spring arm
+	CameraComp->SetupAttachment(SpringArmComp);
+}
+
+// Called when the game starts or when spawned
+void ASCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ASCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/ueGame/Public/SCharacter.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+// Tell current class will use follow classes
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class UEGAME_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASCharacter();
+
+protected:
+
+	// Spring arm
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+
+	// Camera
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
